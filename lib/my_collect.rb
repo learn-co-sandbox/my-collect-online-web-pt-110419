f3986b8fd8cfcd3c00b60d4376e785845c9727a6
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-02 21:33:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
+# array = ["Tim Jones", "Tome Smith", "Jim Campagno"]
+def my_collect(array)
+  # name = []
+i = 0 
+while i < array.length
+my_collect(array) do |name|
+  # name.split(" ").first
+  # yield(array[name])
+  # name.split(" "). first
+end
 
+end
+
+end
